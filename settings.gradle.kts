268e823fc6e13e3e860ab98f82e4949209197ab5
DarKnight <xuhaidong91@gmail.com>
2026-09-14 20:59:32 +0800

    Add E108 Android project settings

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "E108CarGnss_55002613"
+include(":app")
